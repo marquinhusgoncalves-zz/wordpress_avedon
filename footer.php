24770a452429e827f9ef2eccc5f587f2ee17f8bb
Marcus Vinicius Gonçalves <marquinhusgoncalves@hotmail.com>
2015-10-23 14:33:40 -0200

    Atualizações

--- footer.php
+++ footer.php
@@ -22,8 +22,6 @@
 
 	</footer>
   </div> <!-- .container -->
-	
-</body>
 
 	<script src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
@@ -32,4 +30,6 @@
 	<script src = "<?php bloginfo('stylesheet_directory');?>/js/toggle.min.js"></script>
 
 <?php wp_footer(); ?>
+
+</body>
 </html>
--- header.php
+++ header.php
@@ -1,12 +1,7 @@
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
-<!--[if gt IE 8]><!--> <html class = "no-js" lang = ""> <!--<![endif]-->
-<!--[if lt IE 9]><script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script><![endif]-->
 <head>
 	<meta charset = "<?php bloginfo( 'charset' ); ?>">
-	<!-- <meta http-equiv = "X-UA-Compatible" content = "IE=edge,chrome=1"> -->
 	<title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
 	<link rel = "icon" href = "<?php bloginfo('stylesheet_directory');?>/favicon.png">
 	<meta name = "viewport" content = "width=device-width, initial-scale=1">
@@ -19,11 +14,12 @@
 </head>
 
 <body id = "stage">
+  <?php include_once("analyticstracking.php") ?>
   <div class = "container" style = "background: white">
 
 	<header class = "topo_fixed header_menu">
-		<a href = "<?php echo esc_url( home_url( '/' ) ); ?>"><img style = "height: 80px; margin: 15px 3%; position: absolute" src = "<?php bloginfo('stylesheet_directory');?>/img/avedon30anos.svg" alt = "avedon 30 anos"></a>
-		<input	type = "checkbox" id = "sidebartoggler" name = "" value = "">	
+		<a href = "<?php echo esc_url( home_url( '/' ) ); ?>"><img style = "height: 80px; margin: 15px 3%; position: absolute" src = "<?php bloginfo('stylesheet_directory');?>/img/avedon30anos.png" alt = "avedon 30 anos"></a>
+		<input	type = "checkbox" id = "sidebartoggler">	
 			<label class = "toggle visible-xs-block" for = "sidebartoggler"><i class = "fa fa-bars"></i></label>
 			<nav class = "menu_principal sidebar visible-xs-inline-block"><?php wp_nav_menu(array('theme_location' => 'primario',)); ?></nav>
 			<nav class = "menu_principal hidden-xs" style = "margin: 30px 30px 0"><?php wp_nav_menu(array('theme_location' => 'primario',)); ?></nav>
